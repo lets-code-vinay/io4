479b43c467b8a85a3125cd22861e145700ea7f3f
Vinay <83496141+lets-code-vinay@users.noreply.github.com>
2023-03-22 10:55:33 +0530

    assignment added

---
+++ Assignments/2023-03-22/assignment-1.js
@@ -0,0 +1,98 @@
+/**
+ * 1.
+ * If lengths of three sides of a triangle are input through the keyboard
+ * write a program to find the area of the triangle using Heron's formula
+ * Formula:
+ *  semi-perimeter = s =(a + b + c)/2
+ * Area = √s(s−a)(s−b)(s−c)
+ */
+
+/**
+ * 2.
+ * Write  program to receive Cartesian co-ordinates (x,y) of a point and convert thm into
+ * into polar co-ordinates (r,Φ)
+ * 
+ * //calculate the value of r
+    r = sqrt(x*x + y*y);
+
+    //calculate the value of phi
+    //for tan inverse we use atan(value)
+    phi = atan(y/x);
+ */
+
+/**
+ * 3. Write a program to receive values of latitude and longitude,
+ * in degrees, of two places on the earth and outputs the distance between
+ * then in nautical miles. The formula for distance in nautical miles is here
+ */
+
+/**
+ * 4. The current year and the year in which the employee joined
+ * the organization are entered through the keyboard .
+ * If the number of years for which the employee has served
+ * the organization is greater than 3 years then the bonus of
+ * Rs.2500/- is given to the employee.
+ * If the years of service is not greater than 3,
+ * then the program should not do anything.
+ */
+
+/**
+ * 5. In a company if an employee is paid as under:
+ * If his basic salary is less than Rs. 1500,
+ * then HRA=10% of basic salary and DA=90% of basic salary.
+ * If his salary is either equal to or above Rs. 1500,
+ * then HRA = Rs. 500 and DA = 98% of basic salary.
+ * If the employee's salary is input through the keyboard
+ *  write a program to find his gross salary.
+ */
+
+/**
+ * 6.if cost price and selling price of an item are input through the keyboard,
+ * write a program to determine whether the seller has made profit of incurred loss.
+ * Also determine how much profit he made or loss he incurred
+ */
+
+/**
+ * 7. Any integer is input through the keyboard. Write program to find out
+ * whether it is an odd Number or even Number
+ */
+
+/**
+ * 8. Any year is input through the keyboard. Write a program to determine whether
+ * the year is leap year or not.
+ * (Hint: use the %(modulus) operator)
+ */
+
+/**
+ * 9. A five-digit number is entered through the keyboard. Write a program to obtain
+ * the reverse number and determine whether the original and reverse numbers are equal or not.
+ *
+ */
+
+/**
+ * 10. If the ages of Ram, Shyam and Ajay are input through the keyboard,
+ * Write a program to determine the youngest of the three.
+ */
+
+/**
+ * 11. Write a program to find the triangle is valid or not, when the three angle of a triangle
+ * entered through the keyboard. A triangle is valid if all the three angles is equal to 180 degrees.
+ */
+
+/**
+ * 12. Write a program to find the absolute value of a number entered through the keyboard
+ */
+
+/**
+ * 13. The length and breadth of any triangle entered through the keyboard.
+ * Write a program to find if the area of triangle is greater then perimeter of rectangle
+ */
+
+/**
+ * 14. Given three points (x1, y1)(x2, y2)  and (x3, y3) write a program to check
+ * if all the three points fall on one straight line
+ */
+
+/**
+ * 15. Given a point (x,y) write a program to find out if it lies on the x-axis, Y-axis or on the origin
+ */
